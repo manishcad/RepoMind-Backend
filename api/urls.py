125f3e767bdd43f09b491abf220b47c78aad7cc6
Manish Chand <manishtochand@gmail.com>
2026-05-30 22:03:04 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AnalyzeRepoView, ChatRepoView
+from .views import AnalyzeRepoView, ChatRepoView,Home
 
 urlpatterns = [
     path('analyze/', AnalyzeRepoView.as_view(), name='analyze_repo'),
     path('chat/', ChatRepoView.as_view(), name='chat_repo'),
+    path('', Home.as_view(), name='chat_repo'),
 ]
